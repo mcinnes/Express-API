4e6002275783a3be359b2487b3b8712bf8298181
Matt McInnes <matt@mcinnes.me>
2021-04-28 20:07:28 +1000

    updates

--- deploy.sh
+++ deploy.sh
@@ -6,18 +6,24 @@ if ! [ -x "$(command -v aws)" ]; then
   exit 1
 fi
 
+echo Downloading CloudFormation template to ./template.json
+echo ------------------------------------------------------
+echo \n
 #Download Cloudformation file
-curl https://sb-tech-test.s3-ap-southeast-2.amazonaws.com/cloudformation.json --output template.json
+curl -s https://sb-tech-test.s3-ap-southeast-2.amazonaws.com/cloudformation.json --output template.json
 
 #Check that we actually downloaded the file
-if ! [  -f "template.yaml"  ]; then
+if ! [  -f "template.json"  ]; then
   echo 'Error: Failed to download Cloudformation Template' >&2
   exit 1
 fi
 
 #Prompt for Cloudformation parameters
-echo Enter the name of the private key to use for EC2
+echo Enter the name of the private key to use for EC2 \n
+echo Do not add file extension to name
 read privatekey
 
 #Deploy to cloudformation
 aws cloudformation deploy --template-file template.json --stack-name technical-test --parameter-overrides KeyName=$privatekey
+
+aws cloudformation describe-stacks --stack-name technical-test
